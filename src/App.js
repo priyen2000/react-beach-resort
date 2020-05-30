5ab91ecd7d78927d9f2d378dd950f4e5d560dcc6
unknown <priyen2000>
2020-05-30 11:47:46 +0530

    react beach application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "../src/pages/Home";
+import Rooms from "../src/pages/Rooms";
+import SingleRoom from "../src/pages/SingleRoom";
+import Error from "../src/pages/Error";
+import { Route, Switch } from "react-router-dom";
+import Navbar from "../src/components/Navbar";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/rooms/" component={Rooms}></Route>
+          <Route exact path="/rooms/:slug" component={SingleRoom}></Route>
+          <Route component={Error}></Route>
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default App;
